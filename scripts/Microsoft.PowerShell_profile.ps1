ebaa79056df0f3c61a558b261cfcbd22cf33145d
Luke <jangkei@qq.com>
2025-05-03 07:01:20 +0800

    chore: 在PowerShell配置文件中添加Python命令路径
    在Microsoft.PowerShell_profile.ps1中添加了Python命令路径，以便在ESP-IDF开发环境中使用指定的Python版本。

--- scripts/Microsoft.PowerShell_profile.ps1
+++ scripts/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,7 @@
 
 $scriptPath = "d:\dev\dudu-esp32\scripts"
 $initEnvScript = Join-Path $scriptPath "init-env.ps1"
+$PythonCommand = "C:\Espressif\python_env\idf5.3_py3.12_env\Scripts\python.exe"
 
 if (Test-Path $initEnvScript) {
     Write-Host "正在初始化ESP-IDF开发环境..." -ForegroundColor Yellow
